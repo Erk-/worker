e2c1c91ad76b98516d984a6cf177d25ae1928d7b
SpongyBacon <spongybacon@hotmail.co.uk>
2018-03-12 05:20:28 +0000

    message sending

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 #![feature(proc_macro, conservative_impl_trait, generators, try_trait, box_syntax)]
 
-//#[macro_use]
-//extern crate error_chain;
+#[macro_use]
 extern crate log;
 extern crate env_logger;
 extern crate serenity;
@@ -26,20 +25,33 @@ use std::rc::Rc;
 use serenity::gateway::Shard;
 use serenity::model::event::{Event, GatewayEvent, MessageCreateEvent, ReadyEvent};
 use serenity::model::channel::{Message, Channel};
+use serenity::http::Client as SerenityHttpClient;
+use serenity::builder::CreateMessage;
 
 fn main() {
+    env_logger::init();
+
     let mut core = Core::new().expect("Error creating event loop");
     let future = try_main(core.handle()).map_err(Box::new);
     println!("Error running future: {:?}", core.run(future));
 }
+
 #[async]
 fn try_main(handle: Handle) -> Result<(), Error> {
+    let token = format!("Bot {}", env::var("DISCORD_TOKEN").expect("Error no discord token"));
+
+    let mut shard = await!(Shard::new(
+        token.clone(), [0, 1], handle.clone()
+    ))?;
+
     let http_client = Rc::new(HyperClient::configure()
         .connector(HttpsConnector::new(4, &handle)?)
         .build(&handle));
-    
-    let token = env::var("DISCORD_TOKEN").expect("Error no discord token");
-    let mut shard = await!(Shard::new(token, [0, 1], handle.clone()))?;
+
+    let serenity_http = SerenityHttpClient::new(
+        http_client.clone(), handle.clone(), Rc::new(token)
+    );
+    let event_handler = EventHandler::new(handle.clone(), serenity_http);
 
     #[async]
     for message in shard.messages() {
@@ -50,8 +62,8 @@ fn try_main(handle: Handle) -> Result<(), Error> {
         use Event::*;
 
         match event {
-            Dispatch(_, Ready(e)) => on_ready(e),
-            Dispatch(_, MessageCreate(e)) => on_message(e),
+            Dispatch(_, Ready(e)) => event_handler.on_ready(e),
+            Dispatch(_, MessageCreate(e)) => event_handler.on_message(e),
             _ => {
                 // ya nothing else
             }
@@ -61,11 +73,33 @@ fn try_main(handle: Handle) -> Result<(), Error> {
     Ok(())
 }
 
-fn on_ready(_: ReadyEvent) {
-    println!("Connected to discord");
+struct EventHandler {
+    handle: Handle,
+    serenity_http: SerenityHttpClient,
 }
 
-fn on_message(event: MessageCreateEvent) {
-    let msg = event.message;
-    println!("{}#{}: {}", msg.author.name, msg.author.discriminator, msg.content);
+impl EventHandler {
+    fn new(handle: Handle, serenity_http: SerenityHttpClient) -> Self {
+        Self {
+            handle,
+            serenity_http,
+        }
+    }
+
+    fn on_ready(&self, _: ReadyEvent) {
+        println!("Connected to discord");
+    }
+
+    fn on_message(&self, event: MessageCreateEvent) {
+        let msg = event.message;
+        println!("{}#{}: {}", msg.author.name, msg.author.discriminator, msg.content);
+
+        if msg.content == "DABBOT IS GOOD" {
+            let future = self.serenity_http.send_message(msg.channel_id.0, |m| m.content(":pray: real niggas speak the truth"))
+                .map(|m| println!("sent msg {:?}", m))
+                .map_err(|e| println!("Error sending message {:?}", e));
+
+            self.handle.spawn(future);
+        }
+    }
 }
