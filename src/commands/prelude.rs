90b2f9d3b587d5ff2df6c4f647c2ec7865acf5b1
Zeyla Hellyer <hi@zeyla.me>
2018-10-01 19:30:18 -0700

    Re-export error types from command prelude

--- src/commands/prelude.rs
+++ src/commands/prelude.rs
@@ -1,5 +1,6 @@
-pub use super::{CommandResult, Context, Response};
+pub use crate::error::{Error, Result};
 pub use futures::{
     compat::{Future01CompatExt, TokioDefaultSpawner},
     future::{FutureExt, TryFutureExt},
 };
+pub use super::{CommandResult, Context, Response};
