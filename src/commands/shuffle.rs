511b36248782d556a949c889808def0a011dc8d9
Mariell Hoversholm <proximyst@proximyst.com>
2018-10-28 08:52:32 +0100

    Hacky fix to stop queue erring the whole bot

--- src/commands/shuffle.rs
+++ src/commands/shuffle.rs
@@ -5,6 +5,18 @@ pub struct ShuffleCommand;
 impl ShuffleCommand {
     async fn _run(ctx: Context) -> CommandResult {
         let guild_id = ctx.guild_id()?;
+        match await!(ctx.state.queue.get_limit(guild_id, 2)) {
+            Ok(queue) => {
+                if queue.len() < 2 {
+                    return Response::err("To shuffle, you need at least 2 songs in the queue!");
+                } // else {} - If the queue is >= 2, it'll shuffle like usual
+            },
+            Err(why) => {
+                warn!("Error getting the queue of {}: {:?}", guild_id, why);
+
+                return Response::err("There was an error getting your queue.");
+            }
+        }
 
         match await!(ctx.state.queue.shuffle(guild_id)) {
             Ok(()) => Response::text("Shuffled the song queue."),
